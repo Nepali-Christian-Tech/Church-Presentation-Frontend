c51c9d7cf4830aed5d076555a832dcfd562c83c6
sursasundar <sundar.tamang@sursatech.com>
2024-08-31 16:04:27 +0545

    feat: revealJS integrated

--- projects/slideshow/src/app/components/slide-renderer/slide-renderer.component.ts
+++ projects/slideshow/src/app/components/slide-renderer/slide-renderer.component.ts
@@ -1,6 +1,9 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { MaterialModule } from '../../../../../slideshow-lib/src/public-api';
 import { CommonModule } from '@angular/common';
+
+import Reveal from 'reveal.js';
+import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
 
 @Component({
   selector: 'app-slide-renderer',
@@ -9,16 +12,30 @@ import { CommonModule } from '@angular/common';
   templateUrl: './slide-renderer.component.html',
   styleUrl: './slide-renderer.component.scss'
 })
-export class SlideRendererComponent {
+export class SlideRendererComponent implements OnInit {
 
   showInFullscreen = false;
 
   @Output()
   isFullScreen: EventEmitter<boolean> = new EventEmitter();
 
+  ngOnInit(): void {
+    this.initializeRevealJS();
+  }
+
   toggleFullscreen(): void {
     this.showInFullscreen = !this.showInFullscreen;
     this.isFullScreen.emit(this.showInFullscreen);
+  }
+
+  private initializeRevealJS(): void {
+    let deck = new Reveal(
+      {
+        plugins: [Markdown],
+        loop: true,
+      },
+    );
+    deck.initialize();
   }
 
 }
