425946e558e54c25dd325905fe38c36ad10d54c9
Sundar Tamang <sundartamang@Sundars-MacBook-Air.local>
2025-02-09 20:47:32 +0545

    feat: admin tailwind link setup

--- projects/slideshow-admin/src/app/components/admin-dashboard/admin-dashboard.component.ts
+++ projects/slideshow-admin/src/app/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { AdminHeaderComponent } from '../admin-header/admin-header.component';
-import { AdminSidebarComponent } from '../admin-sidebar/admin-sidebar.component';
 
 @Component({
   selector: 'slideshow-admin-dashboard',
   standalone: true,
   imports: [
-    AdminHeaderComponent,
-    AdminSidebarComponent,
     RouterModule
   ],
   templateUrl: './admin-dashboard.component.html',
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require('./projects/slideshow-lib/src/styles/colors');
 
 module.exports = {
   content: [
-    "./projects/**/*.{html,ts}"
+    "./projects/**/*.{html,ts}",
   ],
   theme: {
     extend: {
