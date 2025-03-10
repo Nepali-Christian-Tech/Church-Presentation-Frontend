7bbb8abec4c905bf2464a158e7b9ceae6d9210fc
Sundar Tamang <sundartamang6@gmail.com>
2025-03-10 11:53:20 +0545

    feat: removed admin project

--- projects/slideshow-admin/src/app/components/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './admin-dashboard/admin-dashboard.component';
-export * from './admin-home/admin-home.component';
-export * from './slides/create-slide/create-slide.component';
-export * from './slides/slides.component';
-
